abbe116e1c35560be79fcd154380040bc07a48a1
Thanazio <thanazio@proton.me>
2025-02-12 14:22:08 +0200

    fix: correct API call and add error component

--- app/page.tsx
+++ app/page.tsx
@@ -33,36 +33,41 @@ interface StarWarsApiPeopleResponse {
 export default function Home() {
   const [people, setPeople] = useState<StarWarsCharacter[]>([]);
   const [showSpinner, setShowSpinner] = useState(true);
+  const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
     if (!showSpinner) return;
 
     async function getPeople() {
-      const res = await fetch("https://swapi.dev/api/people/");
-      if (!res.ok) {
-        throw new Error("Unable to contact https://swapi.dev/. Please check your connection, or try again later.");
+      try {
+        const res = await fetch("https://swapi.dev/api/people/");
+        if (!res.ok) {
+          throw new Error(`API Error: ${res.status} - ${res.statusText}`);
+        }
+        const data: StarWarsApiPeopleResponse = await res.json();
+        setPeople(data.results);
       }
-
-      const data: StarWarsApiPeopleResponse = await res.json();
-      setPeople(data.results);
-      setShowSpinner(false);
+      catch (error) {
+        setError("Unable to load data. Check your connection or try again later.");
+      }
+      finally {
+        setShowSpinner(false);
+      }
     }
 
-    setTimeout(getPeople, 3000);
-    // getPeople();
+    getPeople();
   }, []);
 
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        {showSpinner 
-        ? <Spinner />
-        : <ul>
-        {people.map(character =>
-          <li key={character.name}>{character.name}</li>
-        )}
-      </ul>
+        {showSpinner && <Spinner />}
+        {error
+          ? <p className="text-red-500">{error}</p>
+          : <ul>
+            {people.map(character => <li key={character.name}>{character.name}</li>)}
+          </ul>
         }
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
