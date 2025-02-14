8d4089ed8daca7187786f9d00734e4cbd6e70142
Thanazio <thanazio@proton.me>
2025-02-14 23:12:22 +0200

    feat: add filter

--- app/page.tsx
+++ app/page.tsx
@@ -21,13 +21,25 @@ export default function Home() {
   const [showSpinner, setShowSpinner] = useState(true);
   const [error, setError] = useState<string | null>(null);
   const [pageCount, setPageCount] = useState(1);
+  const [charFilter, setCharFilter] = useState("");
 
   const fetchPeople = async (currentPage: number) => {
-    const res = await fetch(
-      `https://swapi.dev/api/people/?page=${currentPage}`
-    );
-    const data = await res.json();
-    setPeople(data.results);
+    const url = `https://swapi.dev/api/people/?page=${currentPage}${
+      charFilter !== "" ? `&search=${charFilter}` : ""
+    }`;
+
+    try {
+      const res = await fetch(url);
+      const data = await res.json();
+      setPeople(data.results);
+    } catch (error) {
+      if (error instanceof Error) {
+        setError(error.message);
+      }
+      setError(
+        "Unable to load data. Check your connection or try again later."
+      );
+    }
   };
 
   const handlePageClick = async (data: { selected: number }) => {
@@ -36,17 +48,18 @@ export default function Home() {
   };
 
   useEffect(() => {
-    if (!showSpinner) return;
-
     async function getPeople() {
+      const url = `https://swapi.dev/api/people/?page=1${
+        charFilter !== "" ? `&search=${charFilter}` : ""
+      }`;
       try {
-        const res = await fetch(`https://swapi.dev/api/people/?page=1`);
+        const res = await fetch(url);
         if (!res.ok) {
           throw new Error(`API Error: ${res.status} - ${res.statusText}`);
         }
+
         const data: StarWarsApiPeopleResponse = await res.json();
         const counter = Math.ceil(data.count / 10);
-        console.log("data:", counter);
         setPageCount(counter);
         setPeople(data.results);
       } catch (error) {
@@ -61,26 +74,49 @@ export default function Home() {
       }
     }
 
-    getPeople();
-  }, []);
+    let debounceHandler: ReturnType<typeof setTimeout>;
+    if (showSpinner) {
+      getPeople();
+    } else {
+      debounceHandler = setTimeout(getPeople, 1500);
+    }
+
+    return () => {
+      clearTimeout(debounceHandler);
+    };
+  }, [charFilter]);
 
   return (
     <div className="flex flex-col h-screen justify-between">
       <main className="flex flex-col items-center justify-center gap-10 mt-10">
         <h1>A long time ago in an API far, far away....</h1>
         {showSpinner && <Spinner />}
-        {error ? (
-          <p className="text-red-500">{error}</p>
-        ) : (
-          <div className="grid sm:grid-cols-1 md:grid-cols-3 gap-8">
-            {people.map((character, index) => (
-              <CharacterCard
-                key={character.name}
-                character={character}
-                index={index}
+        {error && <p className="text-red-500">{error}</p>} 
+        {!showSpinner && !error && (
+          <>
+            <div>
+              <label htmlFor="characterSearch">Character search: </label>
+              <input
+                type="text"
+                name="characterSearch"
+                id="characterSearch"
+                placeholder="Enter a character name"
+                value={charFilter}
+                className="text-black pr-2 pl-2"
+                onChange={(e) => setCharFilter(e.target.value)}
               />
-            ))}
-          </div>
+            </div>
+            <div className="grid sm:grid-cols-1 md:grid-cols-3 gap-8">
+              {people &&
+                people.map((character, index) => (
+                  <CharacterCard
+                    key={character.name}
+                    character={character}
+                    index={index}
+                  />
+                ))}
+            </div>
+          </>
         )}
 
         {!showSpinner && (
@@ -106,7 +142,12 @@ export default function Home() {
         )}
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <p>Made with ❤️ and React by Thanos Dimitriades - 2025 | Powered by <a href="https://swapi.dev/" className="text-yellow-300">The Star Wars API</a></p>
+        <p>
+          Made with ❤️ and React by Thanos Dimitriades - 2025 | Powered by{" "}
+          <a href="https://swapi.dev/" className="text-yellow-300">
+            The Star Wars API
+          </a>
+        </p>
       </footer>
     </div>
   );
